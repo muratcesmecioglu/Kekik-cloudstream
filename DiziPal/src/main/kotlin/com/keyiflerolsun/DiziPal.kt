7ca918110cf7cc443e962184a5b2168e2d3977df
muratcesmecioglu <muratcesmecioglu@gmail.com>
2023-11-09 11:25:26 +0300

    Update DiziPal.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -19,35 +19,22 @@ class DiziPal : MainAPI() {
     override val supportedTypes     = setOf(TvType.TvSeries)
 
     override val mainPage = mainPageOf(
+        "${mainUrl}/diziler/son-bolumler" to "Son Bölümler",
         "${mainUrl}/diziler?kelime=&durum=&tur=1&type=&siralama="  to "Aile",
         "${mainUrl}/diziler?kelime=&durum=&tur=2&type=&siralama="  to "Aksiyon",
-        "${mainUrl}/diziler/son-bolumler" to "Son Bölümler",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data).document
-        val home = if (request.data.contains("/diziler/son-bolumler")) {
-             document.select("div.episode-item").mapNotNull { it.toSearchResultLast() }
-        } else {
-             document.select("article.type2 ul li").mapNotNull { it.toSearchResult() }
-        }
+        val home = document.select("div.episode-item").mapNotNull { it.toSearchResultLast() }
         //val home     = document.select("article.type2 ul li").mapNotNull { it.toSearchResult() }
 
         return newHomePageResponse(request.name, home, hasNext=false)
     }
-
-    private fun Element.toSearchResultLast(): SearchResponse? {
-        val title1     = this.selectFirst("div.name")?.text() ?: return null
-        val title2     = this.selectFirst("div.episoed")?.text() ?: return null
-        var title     = title1 + " " + title2
-        val href      = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
-
-        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
-    }
     
     private fun Element.toSearchResult(): SearchResponse? {
-        val title     = this.selectFirst("span.title")?.text() ?: return null
+        //val title     = this.selectFirst("span.title")?.text() ?: return null
+        val title     = this.selectFirst("div.title")?.text() ?: return null
         val href      = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
 
